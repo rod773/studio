3e0ab82cdd3afcfcfac6e5e85d5091caeea97789
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-11 15:53:57 +0200

    home component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
+import { dark } from "./styles/Themes";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRef } from "react";
+import Home from "./sectioms/Home";
 
 function App() {
+  const containerRef = useRef(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App" ref={containerRef}>
+      <GlobalStyles />
+      <ThemeProvider theme={dark}>
+        <LocomotiveScrollProvider
+          options={{
+            smooth: true,
+            // ... all available Locomotive Scroll instance options
+          }}
+          watch={
+            [
+              //..all the dependencies you want to watch to update the scroll.
+              //  Basicaly, you would want to watch page/location changes
+              //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+            ]
+          }
+          containerRef={containerRef}
         >
-          Learn React
-        </a>
-      </header>
+          <main data-scroll-container ref={containerRef}>
+            <Home />
+          </main>
+        </LocomotiveScrollProvider>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/sectioms/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  position: relative;
+  min-height: 100vh;
+`;
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
