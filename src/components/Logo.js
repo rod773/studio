4bb5c705566bdcbd657af7a55c6599efdab04a2c
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-12 19:23:26 +0200

    animation logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -12,6 +12,11 @@ const Container = styled.div`
   color: ${(props) => props.theme.text};
   z-index: 5;
 
+  a {
+    display: flex;
+    align-items: flex-end;
+  }
+
   svg {
     width: 4rem;
     height: auto;
@@ -27,6 +32,44 @@ const Container = styled.div`
   }
 `;
 
+const Text = styled(motion.span)`
+  font-size: ${(props) => props.theme.fontlg};
+  color: ${(props) => props.theme.text};
+  padding-bottom: 0.5rem;
+`;
+
+const textVariants = {
+  hidden: {
+    opacity: 0,
+    x: -50,
+  },
+  visible: {
+    opacity: 1,
+    x: -5,
+
+    transition: {
+      duration: 2,
+      delay: 2,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const pathVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
+
 export const Logo = () => {
   return (
     <Container>
@@ -41,9 +84,17 @@ export const Logo = () => {
         >
           <g></g>
           <g>
-            <path d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z" />
+            <motion.path
+              variants={pathVariants}
+              initial="hidden"
+              animate="visible"
+              d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z"
+            />
           </g>
         </svg>
+        <Text variants={textVariants} initial="hidden" animate="visible">
+          Wibe Studio
+        </Text>
       </Link>
     </Container>
   );
