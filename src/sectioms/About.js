b89ec5fa3c483445bab26b2643e53184bdd51835
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-19 12:26:16 +0200

    motion title

--- src/sectioms/About.js
+++ src/sectioms/About.js
@@ -12,12 +12,25 @@ const Title = styled.h1`
   font-size: ${(props) => props.theme.fontBig};
   font-family: "Kaushan Script";
   font-weight: 300;
+  position: absolute;
+  top: 1rem;
+  left: 5%;
+  z-index: 5;
 `;
+
+const Left = styled.div``;
 
 const About = () => {
   return (
     <Section>
-      <Title>About Us</Title>
+      <Title
+        data-scroll
+        data-scroll-speed="-2"
+        data-scroll-direction="horizontal"
+      >
+        About Us
+      </Title>
+      <Left>Text</Left>
     </Section>
   );
 };
