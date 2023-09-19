8647e7d6b4d22d54c89dd1466186fef4aebfd7ed
rod773 <104315743+rod773@users.noreply.github.com>
2023-09-19 12:00:23 +0200

    about component

---
+++ src/sectioms/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = styled.section`
+  position: relative;
+  min-height: 100vh;
+  overflow: hidden;
+`;
+
+const About = () => {
+  return <Section>About</Section>;
+};
+
+export default About;
